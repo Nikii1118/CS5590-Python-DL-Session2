c3a64b5fd3279cf11d169acc171a16ae993bf6d5
Nikii1118 <47010164+Nikii1118@users.noreply.github.com>
2019-02-18 11:01:02 -0600

    ICP4
    Q3 is implemented in Q1 and Q2 only.

---
+++ Source/ICP4_Q2.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+
+#importing the dataset into iris
+iris= pd.read_csv('iris.csv')
+print(iris.head()) #it automatically shows some top 5 rows of the dataset
+print(iris.shape) #it tells us there are 150 rows and 5 columns
+print(iris['class'].unique()) # this result gives all the unique class names
+print(iris.describe())
+# dividing the dataset into training nad testing part
+X=iris.drop('class', axis=1) # X represents all the attributes from data except class attribute in data
+y= iris['class'] # y represents class attribute
+# we split the data  for train and testing
+X_train, X_test, y_train, y_test= train_test_split(X,y,test_size= 0.20)
+# we fit the model into svc support vector classifier
+
+from sklearn.svm import SVC
+svcclassifier= SVC(kernel='linear') # we have use linear svm
+svcclassifier.fit(X_train,y_train)
+# to prdict the data
+y_pred=svcclassifier.predict(X_test)
+# Evaluating and algorithm
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+
+print("Accuracy : %0.2f:" ,accuracy_score(y_test,y_pred))
